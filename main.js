5ef106c011e98e85140ecf7fe3fb4e2d8ff7b8c5
Nikita rawat <72080644+nikita172@users.noreply.github.com>
2022-08-16 11:41:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,33 +4,32 @@ const calcBtn = document.querySelector(".calcBtn")
 const percentShow = document.querySelector(".percentShow")
 const closeBtn = document.querySelector(".closeBtn")
 const checkAgain = document.querySelector(".checkAgainButton")
+const displayans = document.querySelector(".displayAns")
 let temp=false;
+
 function openAns(){
-    console.log("hello")
+    console.log("hello")    
     if(temp===false){
         temp=true;
-        span.style.display="block";
+        span.style.display="block";        
     }else{
         temp=false;
         span.style.display="none";
     }
 }
 
-function showPercentage(){
-    console.log("hello")
-    
+function showPercentage(){   
+    const percent = Math.floor((Math.random()*100)+1)
+    displayans.textContent=percent + "%";
     percentShow.style.display="block"
     closeBtn.addEventListener("click" ,close)
     checkAgain.addEventListener("click" ,close)
 
-    
-
 }
+
 function close(){
     percentShow.style.display="none"
 }
-
-
 
 calcBtn.addEventListener("click" , showPercentage)
 scrolldown.addEventListener("click",openAns)
